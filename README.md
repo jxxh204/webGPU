a882fb43a730daba663e8cf6886a3ea6534f4a49
JaeHwan <ahhancom@gmail.com>
2022-01-16 17:40:05 +0900

    v0.0.1:2 create basic house

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 ## 1/11
 - v0.0.1:0 setup
-- v0.0.2:0 create box
+- v0.0.1:1 create box
+
+## 1/16
+ - v0.0.1:2 create basic house
--- src/app.js
+++ src/app.js
@@ -7,11 +7,17 @@
     
         const scene = new BABYLON.Scene(engine);  
 
-        BABYLON.MeshBuilder.CreateBox("box", {})
-
         const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
         camera.attachControl(canvas, true);
         const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+
+        const box = BABYLON.MeshBuilder.CreateBox("box", {});
+        box.position.y = 0.5;
+        const roof = BABYLON.MeshBuilder.CreateCylinder("roof", {diameter: 1.3, height: 1.2, tessellation: 3});
+        roof.scaling.x = 0.75;
+        roof.rotation.z = Math.PI / 2;
+        roof.position.y = 1.22;
+        const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:10, height:10});
 
         return scene;
     };
